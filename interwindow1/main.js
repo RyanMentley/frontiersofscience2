9c6b7a5bc8067384b54f35c1c24d70030e5dcf0c
CDeLorme <cdelorme@gmail.com>
2013-03-14 17:04:23 -0400

    Updated index and main to act as a control page and activate the demo.

--- interwindow1/main.js
+++ interwindow1/main.js
@@ -1,5 +1,24 @@
 (function($) {
 
-	// Main Window Closure
+	// Namespace
+	var ns = {};
+
+/*
+
+Method 1:
+
+Spawn Second Window.
+Send operations to that window with direct calls.
+Test triggering events from scripts available on that window.
+
+Method 2:
+
+Spawn Second Window.
+Use sendMessage and receiveMessage to handle event triggers.
+
+*/
+
+
+	console.log(window.length);
 
 })(jQuery);
